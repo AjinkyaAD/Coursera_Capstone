8b8978f5b435907104cdce54a6a464d9932bfb77
AjinkyaAD <48825415+AjinkyaAD@users.noreply.github.com>
2019-03-22 16:53:10 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,13 @@
+
+# coding: utf-8
+
+# Hi This notebook will be used for coursera capstone project
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+print ("Hello Capstone Project Course!")
+
